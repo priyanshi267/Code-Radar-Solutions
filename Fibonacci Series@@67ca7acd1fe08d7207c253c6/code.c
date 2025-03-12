42d1e25b4b3f9b4f4eb8c50ce192bdcf180d4ab3
Priyanshi Gupta <priynshiigupta@gmail.com>
2025-03-12 20:31:37 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int fibonacciSeries(int n);
+void fibonacciSeries(int n);
 
 int main() {
     int n;
@@ -9,16 +9,16 @@ int main() {
     return 0;
 }
 
-int fibonacciSeries(int n) {
+void fibonacciSeries(int n) {
     if(n==0) {
         return 0;
     }
     if(n==1) {
         return 1;
     }
-    // if(n<=0) {
-    //     printf("Invalid input\n");
-    // }
+    if(n<=0) {
+        printf("Invalid input\n");
+    }
     int fibonacciSeriesN = fibonacciSeries(n-1) + fibonacciSeries(n-2);
     printf("%d", fibonacciSeriesN);
     return fibonacciSeriesN;
